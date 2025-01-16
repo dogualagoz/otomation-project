36249ccb590b124e02c3733268933f45baba30b0
dogualagoz <alagozdogu@gmail.com>
2025-01-16 15:58:44 +0300

    multiplying is working

---
+++ utils_photoshop.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+
+
+def process_image_with_photoshop(tiff_file, image_file, output_folder, barcode_text1, barcode_text2):
+    """
+    Photoshop işlemlerini ExtendScript ile gerçekleştiren fonksiyon.
+    """
+    # Photoshop Script'in yolu
+    photoshop_script = os.path.join(os.getcwd(), "scripts", "photoshop_script.jsx")
+
+    # AppleScript komutunu oluştur
+    apple_script = f"""
+        osascript -e 'tell application "Adobe Photoshop 2025"
+            do javascript file "{photoshop_script}" with arguments {{"{tiff_file}", "{image_file}", "{output_folder}", "{barcode_text1}", "{barcode_text2}"}}
+        end tell'
+    """
+
+    # Komutu çalıştır
+    try:
+        subprocess.run(apple_script, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"{image_file} işlenirken hata oluştu: {e}")
+        return False
+
+    return True
