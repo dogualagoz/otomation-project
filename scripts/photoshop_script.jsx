79128e47cdba932c99ee69a8df07b9506390f4ad
dogualagoz <alagozdogu@gmail.com>
2025-01-17 01:58:56 +0300

    working script on asd.jsx trying to process 10 BARCODES

--- scripts/photoshop_script.jsx
+++ scripts/photoshop_script.jsx
@@ -33,7 +33,16 @@ doc.paste();
 var pastedLayer1 = doc.activeLayer;
 alert("Resim TIFF dosyasına yapıştırıldı.");
 
-// 4. Resmi Rectangle 1 ile hizala ve boyutlandır
+// 4. Dosya adını al
+var baseName;
+try {
+    baseName = decodeURIComponent(imageFile.fsName).match(/([^\/]+)(?=\.\w+$)/)[0];
+} catch (e) {
+    throw new Error("Dosya adından baseName alınırken hata oluştu: " + e.message);
+}
+alert("Dosya adı başarıyla alındı: " + baseName);
+
+// 5. Resmi Rectangle 1 ile hizala ve boyutlandır
 var rect1;
 try {
     rect1 = doc.artLayers.getByName("Rectangle 1");
@@ -47,7 +56,7 @@ pastedLayer1.resize(((rect1Bounds[2] - rect1Bounds[0]) / pastedLayer1.bounds[2])
 pastedLayer1.translate(rect1Bounds[0] - pastedLayer1.bounds[0], rect1Bounds[1] - pastedLayer1.bounds[1]);
 alert("Resim Rectangle 1 ile hizalandı.");
 
-// 5. Resmi kopyala ve Rectangle 1 Copy ile hizala
+// 6. Resmi kopyala ve Rectangle 1 Copy ile hizala
 var pastedLayer2 = pastedLayer1.duplicate();
 var rect1Copy;
 try {
@@ -62,40 +71,29 @@ pastedLayer2.resize(((rect1CopyBounds[2] - rect1CopyBounds[0]) / pastedLayer2.bo
 pastedLayer2.translate(rect1CopyBounds[0] - pastedLayer2.bounds[0], rect1CopyBounds[1] - pastedLayer2.bounds[1]);
 alert("Resmin kopyası Rectangle 1 copy ile hizalandı.");
 
-// 6. Layer sıralamasını kontrol et ve düzelt
+// 7. Layer sıralamasını kontrol et ve düzelt
 try {
-    // Katman 1 ve Katman 1 kopya en alta taşınıyor
     pastedLayer1.move(doc.artLayers[doc.artLayers.length - 1], ElementPlacement.PLACEAFTER);
     pastedLayer2.move(doc.artLayers[doc.artLayers.length - 1], ElementPlacement.PLACEAFTER);
-    alert("Katman sıralaması düzenlendi. Katman 1 ve Katman 1 kopya en alta taşındı.");
+    alert("Katman sıralaması düzenlendi. Katman 1 ve Katman 1 copy en alta taşındı.");
 } catch (e) {
     alert("Katman sıralaması yapılırken hata oluştu: " + e.message);
     throw new Error("Katman sıralaması yapılamadı.");
 }
 
-// 7. Barkod metinlerini güncelle
+// 8. Barkod metinlerini güncelle
 try {
-    var barcodeLayer1 = doc.artLayers.getByName("BARKOD");
-    barcodeLayer1.textItem.contents = barcodeText;
-    alert("BARKOD metni güncellendi: " + barcodeText);
+    for (var i = 1; i <= 10; i++) {
+        var barcodeLayer = doc.artLayers.getByName("BARKOD " + i);
+        barcodeLayer.textItem.contents = barcodeText;
+        alert("BARKOD " + i + " metni güncellendi: " + barcodeText);
+    }
 } catch (e) {
-    alert("BARKOD katmanı bulunamadı: " + e.message);
-    throw new Error("BARKOD katmanı bulunamadı.");
+    alert("BARKOD katmanları güncellenirken hata oluştu: " + e.message);
+    throw new Error("BARKOD katmanları güncellenirken hata oluştu.");
 }
 
-try {
-    var barcodeLayer2 = doc.artLayers.getByName("BARKOD 2");
-    barcodeLayer2.textItem.contents = barcodeText;
-    alert("BARKOD 2 metni güncellendi: " + barcodeText);
-} catch (e) {
-    alert("BARKOD 2 katmanı bulunamadı: " + e.message);
-    throw new Error("BARKOD 2 katmanı bulunamadı.");
-}
-
-// 8. Dosyaları kaydet
-var baseName = decodeURIComponent(imageFile.name).match(/([^\/]+)(?=\.\w+$)/)[0];
-
-// JPEG olarak kaydet
+// 9. Dosyaları kaydet
 var jpegSaveFile = File(outputFolder + "/" + baseName + "_result.jpg");
 var jpegOptions = new JPEGSaveOptions();
 jpegOptions.quality = 12;
@@ -107,7 +105,6 @@ try {
     throw new Error("JPEG kaydetme sırasında hata oluştu.");
 }
 
-// TIFF olarak kaydet
 var tiffSaveFile = File(outputFolder + "/" + baseName + "_result.tif");
 var tiffOptions = new TiffSaveOptions();
 tiffOptions.layers = true;
@@ -119,7 +116,7 @@ try {
     throw new Error("TIFF kaydetme sırasında hata oluştu.");
 }
 
-// 9. TIFF dosyasını kapat
+// 10. TIFF dosyasını kapat
 try {
     doc.close(SaveOptions.DONOTSAVECHANGES);
     alert("TIFF dosyası kapatıldı.");
