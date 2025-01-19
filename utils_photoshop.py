b11c7ede5ab212ca1c974110779468506094c1e8
dogualagoz <alagozdogu@gmail.com>
2025-01-19 17:05:08 +0300

    App bugs fixed. release/1.0.1

--- utils_photoshop.py
+++ utils_photoshop.py
@@ -1,13 +1,20 @@
 import os
 import subprocess
+import sys
 
+def get_base_path():
+    """PyInstaller ile paketlenmiş dosyalar için çalışma yolu."""
+    if getattr(sys, 'frozen', False):  # PyInstaller ile paketlenmiş mi?
+        return sys._MEIPASS  # Geçici çalışma dizini
+    else:
+        return os.path.dirname(os.path.abspath(__file__))
 
 def process_image_with_photoshop(tiff_file, image_file, output_folder, barcode_text):
     """
     Photoshop işlemlerini ExtendScript ile gerçekleştiren fonksiyon.
     """
     # Photoshop Script'in yolu
-    photoshop_script = os.path.join(os.getcwd(), "scripts", "photoshop_script.jsx")
+    photoshop_script = os.path.join(get_base_path(), "scripts", "photoshop_script.jsx")
 
     # AppleScript komutunu oluştur
     apple_script = f"""
