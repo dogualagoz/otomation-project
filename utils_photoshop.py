4864a41a2be5219f781803a237bf7200a4821394
dogualagoz <alagozdogu@gmail.com>
2025-01-23 21:29:13 +0300

    logs are removed. release/1.2.1

--- utils_photoshop.py
+++ utils_photoshop.py
@@ -1,14 +1,6 @@
 import os
 import subprocess
 import sys
-import logging
-
-# Loglama ayarları
-logging.basicConfig(
-    filename=os.path.join(os.getcwd(), "logs", "errors.log"),
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-)
 
 def get_base_path():
     """PyInstaller ile paketlenmiş dosyalar için çalışma yolu."""
@@ -24,12 +16,6 @@ def process_image_with_photoshop(tiff_file, image_file, output_folder, barcode_t
     # Photoshop Script'in yolu
     photoshop_script = os.path.join(get_base_path(), "scripts", "photoshop_script.jsx")
 
-    logging.info(f"Tiff File: {tiff_file}")
-    logging.info(f"Image File: {image_file}")
-    logging.info(f"Output Folder: {output_folder}")
-    logging.info(f"Barcode Text: {barcode_text}")
-    logging.info(f"Beden Type: {beden_type}")
-
     # AppleScript komutunu oluştur
     apple_script = f"""
         osascript -e 'tell application "Adobe Photoshop 2025"
@@ -37,13 +23,9 @@ def process_image_with_photoshop(tiff_file, image_file, output_folder, barcode_t
         end tell'
     """
 
-    logging.info(f"AppleScript Command: {apple_script}")
-
     # Komutu çalıştır
     try:
         subprocess.run(apple_script, shell=True, check=True)
-        logging.info(f"{image_file} başarıyla işlendi.")
         return True
-    except subprocess.CalledProcessError as e:
-        logging.error(f"{image_file} işlenirken hata oluştu: {e}")
+    except subprocess.CalledProcessError:
         return False
